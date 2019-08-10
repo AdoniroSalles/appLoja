d6220f7f7f5f61244af39719fb580557c88279a4
adoniro <adonirosalles@gmail.com>
2019-08-10 17:07:41 -0300

    inserindo card de resumo do pedido

--- lib/Screens/cart_screen.dart
+++ lib/Screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:loja_virtual/Screens/login_screen.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_models.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_cart.dart';
 import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -80,7 +81,8 @@ class CartScreen extends StatelessWidget {
                   ).toList()
                 ),
                 DiscountCart(),
-                ShipCard()
+                ShipCard(),
+                CartPrice((){})
               ],
             );
           }
